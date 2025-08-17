73457efd71ccc46364922712768cc0a472a73c17
Luke Na <narukeu@outlook.com>
2025-08-17 16:30:00 +0800

    feat: 新增数组相关文章

--- .vitepress/sidebars/codes.ts
+++ .vitepress/sidebars/codes.ts
@@ -3,6 +3,20 @@ import type { DefaultTheme } from "vitepress";
 export const codesSidebar: DefaultTheme.SidebarItem[] = [
   { text: "简介", items: [{ text: "代码片段", link: "/codes" }] },
   {
+    text: "数组",
+    items: [
+      { text: "对象数组指定键求和", link: "/codes/array-sum" },
+      {
+        text: "根据选择数据获取标签",
+        link: "/codes/array-get-value-by-select-data"
+      },
+      {
+        text: "根据 id 列表从对象数组中过滤出项并返回匹配项和在其在原数组的索引",
+        link: "/codes/filter-array-with-index-by-id"
+      }
+    ]
+  },
+  {
     text: "随机",
     items: [{ text: "简易版生成 UID 函数", link: "/codes/simple-uid" }]
   },
