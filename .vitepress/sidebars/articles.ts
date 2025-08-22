dd5a23dcf0d0d36cc0a47fb49be8e6179ae56959
Blue Hat <narukeu@outlook.com>
2025-08-23 04:12:27 +0800

    feat: 新增文章

--- .vitepress/sidebars/articles.ts
+++ .vitepress/sidebars/articles.ts
@@ -1,5 +1,14 @@
 import type { DefaultTheme } from "vitepress";
 
 export const articlesSidebar: DefaultTheme.SidebarItem[] = [
-  { text: "简介", link: "/articles/" }
+  { text: "简介", link: "/articles/" },
+  {
+    text: "Linux",
+    items: [
+      {
+        text: "使用 Systemd 防止移动硬盘休眠",
+        link: "/articles/linux-keep-usb-alive"
+      }
+    ]
+  }
 ];
