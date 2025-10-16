adf098872d6cda709c4fe5585a63ad3e531e4fce
Luke Na <narukeu@outlook.com>
2025-10-16 14:10:52 +0800

    feat: 精简微服务项目中鉴权逻辑标题，更新相关文档内容

--- .vitepress/sidebars/spring.ts
+++ .vitepress/sidebars/spring.ts
@@ -4,8 +4,6 @@ export const springSidebar: DefaultTheme.SidebarItem[] = [
   { text: "简介", items: [{ text: "设计文档", link: "/spring" }] },
   {
     text: "微服务",
-    items: [
-      { text: "微服务项目中鉴权逻辑和分工", link: "/spring/microservice-auth" }
-    ]
+    items: [{ text: "微服务项目中鉴权逻辑", link: "/spring/microservice-auth" }]
   }
 ];
